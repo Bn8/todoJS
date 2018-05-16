b20d99223cc4ccb677d64049ca06a08bbb7cd67f
niv <braniv@gmail.com>
2018-05-16 13:05:39 +0300

    checkbox save via html plug

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 
+$.when(
+    $.getScript( "/cookies.js" ),
+    $.Deferred(function( deferred ){
+        $( deferred.resolve );
+    })
+).done(function(){
+    // finished loading all scripts
+});
+
 var badd = $("#b-add");
 var bsave = $("#save");
 var bload = $("#load");
 var bfilter = $("#filter");
+
+var todolist = $("#todolist");
+
+var cookie_expire_days = 1;
 
 var filter_flag = 0;
 
@@ -16,8 +29,11 @@ function addTodo(todotext) {
         txt.append(String(todotext));
     }
 
-    badd.before(div.append(checkdone).append(txt));
-    badd.before(divider);
+    todolist.append(div.append(checkdone).append(txt));
+    todolist.append(divider);
+
+    //badd.before(div.append(checkdone).append(txt));
+    //badd.before(divider);
 }
 
 function setTextareaAutoResize() {
@@ -29,20 +45,30 @@ function setTextareaAutoResize() {
     });
 }
 
+
+/**
+ * V3 - same as V2 but add the default "checked" when we save a checked checkbox, because whats the point of checking a checkbox if saving doesnt checks the checkboxing checkbox.
+ * security risk from V2 applies here as well.
+ */
 function save() {
-    var alltexts = [];
-    $('.txt').each(function (indexInArray, valueOfElement) { 
-        alltexts.push($(this).text());
+    // this actually sets an HTML attribute to specifically be checked="checked" because otherwise its hidden for some reason probably versioning
+    $(".todo").each(function() {
+        var checkbox =  $(this).find('.checkdone');
+        if(checkbox.is(':checked')) { // yes this is weird solution but a simple one
+            checkbox.attr('checked', true);
+        }
     });
-    setCookie("todo-list", alltexts, 1);
+    var alltexts = $("#todolist").html();
+    setCookie("todo-list", alltexts, cookie_expire_days);
 }
 
 function load() {
     var alltexts = getCookie("todo-list");
-    alltexts.split(",").forEach(element => {
-        addTodo(element);
-    });
-}
+    if(alltexts === ""){ return; } // no cookie
+    var dom_todo = $.parseHTML(alltexts);
+    $("#todolist").html(dom_todo); // append or html (=inner replaceWith), can add this to user configuration or make popup to ask "r u sure?"
+} 
+
 
 function filter() {
     var disable_filter = filter_flag == 1 ? true : false;
@@ -61,29 +87,6 @@ function setFilterHidden(todo, disable_filter) {
     } else if(checkbox.is(':checked')) {
         todo.hide();
     }
-}
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
 }
 
 // once document loaded completely 
